20e0bb260050271c877624dce846de3be712e37f
Tim Smart <tim.smart@arisechurch.com>
2021-11-29 14:21:21 +1300

    Add persistIList extension

--- lib/src/persist.dart
+++ lib/src/persist.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:fast_immutable_collections/fast_immutable_collections.dart';
 import 'package:fpdart/fpdart.dart';
 import 'package:offset_iterator/offset_iterator.dart';
 import 'package:offset_iterator/src/offset_iterator.dart';
@@ -33,13 +34,27 @@ void Function(T) _writeCache<T>(
       cache?[key] = some(value);
     };
 
-extension PersistExtension<T> on OffsetIterator<T, dynamic> {
-  OffsetIterator<T, int> persist({
+bool iListSublistEquality<T>(IList<T> prev, IList<T> next) {
+  final prevLength = prev.length;
+  final nextLength = next.length;
+
+  if (prevLength == nextLength) {
+    return prev == next;
+  } else if (prevLength < nextLength) {
+    return false;
+  }
+
+  return prev.sublist(0, nextLength) == next;
+}
+
+extension PersistExtension<T> on OffsetIterator<T> {
+  OffsetIterator<T> persist({
     required Storage storage,
+    Map<String, dynamic>? cache,
     required String key,
+    bool Function(T prev, T next)? equals,
     required Object? Function(T) toJson,
     required T Function(Object?) fromJson,
-    Map<String, dynamic>? cache,
   }) {
     key = 'OffsetIterator_$key';
     final seed = _readCache(storage, cache, key, fromJson);
@@ -48,5 +63,34 @@ extension PersistExtension<T> on OffsetIterator<T, dynamic> {
       _writeCache(storage, cache, key, toJson),
       seed: seed.toNullable(),
     );
+  }
+}
+
+extension PersistIListExtension<T> on OffsetIterator<IList<T>> {
+  OffsetIterator<IList<T>> persistIList({
+    required Storage storage,
+    Map<String, dynamic>? cache,
+    required String key,
+    required Object? Function(T) toJson,
+    required T Function(Object?) fromJson,
+  }) {
+    key = 'OffsetIterator_$key';
+    final seed = _readCache(
+      storage,
+      cache,
+      key,
+      (json) => IList.fromJson(json, fromJson),
+    );
+    final write = _writeCache<IList<T>>(
+      storage,
+      cache,
+      key,
+      (l) => l.toJson(toJson),
+    );
+
+    return distinct(
+      equals: iListSublistEquality,
+      seed: seed.toNullable(),
+    ).tap(write);
   }
 }
