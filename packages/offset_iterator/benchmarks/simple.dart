7eee0616e13d6a955ba6c15c944b8159cc6ff54a
Tim Smart <tim.smart@arisechurch.com>
2021-12-02 16:01:24 +1300

    Add single subscriber stream benchmark

--- packages/offset_iterator/benchmarks/simple.dart
+++ packages/offset_iterator/benchmarks/simple.dart
@@ -5,9 +5,9 @@ void main() async {
   final numbers = Iterable.generate(1000000);
 
   (await asyncBenchmark('OffsetIterator.range[k]', () async {
-    await OffsetIterator.range(0, end: 1000000).run();
+    await OffsetIterator.range(0, end: numbers.last).run();
   }))
-      .report(units: 1000000);
+      .report(units: numbers.last);
 
   (await asyncBenchmark('OffsetIterator.fromIterable[k]', () async {
     await OffsetIterator.fromIterable(numbers).run();
@@ -15,6 +15,11 @@ void main() async {
       .report(units: numbers.length);
 
   (await asyncBenchmark('Stream.fromIterable[k]', () async {
+    await Stream.fromIterable(numbers).forEach((_) {});
+  }))
+      .report(units: numbers.length);
+
+  (await asyncBenchmark('Stream.fromIterable[k] broadcast', () async {
     await Stream.fromIterable(numbers).asBroadcastStream().forEach((_) {});
   }))
       .report(units: numbers.length);
