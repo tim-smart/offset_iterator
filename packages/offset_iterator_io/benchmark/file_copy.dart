c4fbcd43aa4437f13b875f2a861dd0a4757c9452
Tim Smart <tim.smart@arisechurch.com>
2021-12-04 20:27:04 +1300

    Benchmark improvements

--- packages/offset_iterator_io/benchmark/file_copy.dart
+++ packages/offset_iterator_io/benchmark/file_copy.dart
@@ -11,18 +11,18 @@ void main(List<String> args) async {
   final megabytes = (file.statSync().size / 1024 / 1024).round();
 
   const settings = BenchmarkSettings(
+    warmupTime: Duration(seconds: 2),
     minimumRunTime: Duration(seconds: 10),
   );
 
-  (await asyncBenchmark('OffsetIterator writeToFile', () async {
+  (await asyncBenchmark('OffsetIterator.writeToFile', () async {
     await fileIterator(file).writeToFile(destination).run();
     await destination.delete();
   }, settings: settings))
       .report(units: megabytes);
 
   (await asyncBenchmark('File.openWrite', () async {
-    final sink = destination.openWrite();
-    await file.openRead().pipe(sink);
+    await file.openRead().pipe(destination.openWrite());
     await destination.delete();
   }, settings: settings))
       .report(units: megabytes);
