b72c857bf662c30a992a496e992eb34a1e85c50f
Tim Smart <tim.smart@arisechurch.com>
2022-09-21 09:50:24 +1200

    Add `iteratorFutureValue` to offset_iterator_nucleus

--- packages/offset_iterator_nucleus/lib/offset_iterator_nucleus.dart
+++ packages/offset_iterator_nucleus/lib/offset_iterator_nucleus.dart
@@ -65,6 +65,65 @@ class OffsetIteratorFutureValue<T> extends OffsetIteratorValue<FutureValue<T>> {
   Option<T> get data => O.fromNullable(value.dataOrNull);
 }
 
+OffsetIteratorFutureValue<T> iteratorFutureValue<T>(
+  AtomContext<OffsetIteratorFutureValue<T>> get,
+  Atom<OffsetIterator<T>> parent, {
+  int initialDemand = 1,
+}) {
+  final iterator = get(parent);
+
+  var disposed = false;
+  get.onDispose(() => disposed = true);
+
+  bool shouldPullMore(int remaining) =>
+      !disposed && remaining > 0 && !iterator.drained;
+
+  late Future<void> Function() maybePull;
+
+  Future<void> doPull(int remaining) =>
+      Future.value(iterator.pull()).then((value) {
+        final pullMore = shouldPullMore(remaining - 1);
+
+        get.setSelf(OffsetIteratorFutureValue(
+          value
+              .p(O.map(FutureValue.data))
+              .p(O.alt(() => O.fromNullable(get.previousValue?.value)))
+              .p(O.getOrElse(FutureValue.loading)),
+          iterator.hasMore(),
+          pullMore,
+          maybePull,
+        ));
+
+        return pullMore ? doPull(remaining - 1) : Future.value();
+      }).catchError((err, stack) {
+        get.setSelf(OffsetIteratorFutureValue(
+          FutureValue.error(err, stack),
+          iterator.hasMore(),
+          false,
+          maybePull,
+        ));
+      });
+
+  maybePull = () {
+    if (!shouldPullMore(1)) return Future.value();
+    return doPull(1);
+  };
+
+  if (shouldPullMore(initialDemand)) {
+    doPull(initialDemand);
+  }
+
+  return OffsetIteratorFutureValue(
+    iterator.value.p(O.fold(
+      () => const FutureValue.loading(),
+      (v) => FutureValue.data(v),
+    )),
+    iterator.hasMore(),
+    initialDemand > 0,
+    maybePull,
+  );
+}
+
 /// Pulls an [OffsetIterator] on demand, and exposes the most recently pulled
 /// [OffsetIteratorFutureValue].
 AtomWithParent<OffsetIteratorFutureValue<T>, Atom<OffsetIterator<T>>>
@@ -72,60 +131,14 @@ AtomWithParent<OffsetIteratorFutureValue<T>, Atom<OffsetIterator<T>>>
   AtomReader<OffsetIterator<T>> create, {
   int initialDemand = 1,
 }) =>
-        atomWithParent(iteratorOnlyAtom(create), (get, parent) {
-          final iterator = get(parent);
-
-          var disposed = false;
-          get.onDispose(() => disposed = true);
-
-          bool shouldPullMore(int remaining) =>
-              !disposed && remaining > 0 && !iterator.drained;
-
-          late Future<void> Function() maybePull;
-
-          Future<void> doPull(int remaining) =>
-              Future.value(iterator.pull()).then((value) {
-                final pullMore = shouldPullMore(remaining - 1);
-
-                get.setSelf(OffsetIteratorFutureValue(
-                  value
-                      .p(O.map(FutureValue.data))
-                      .p(O.alt(() => O.fromNullable(get.previousValue?.value)))
-                      .p(O.getOrElse(FutureValue.loading)),
-                  iterator.hasMore(),
-                  pullMore,
-                  maybePull,
-                ));
-
-                return pullMore ? doPull(remaining - 1) : Future.value();
-              }).catchError((err, stack) {
-                get.setSelf(OffsetIteratorFutureValue(
-                  FutureValue.error(err, stack),
-                  iterator.hasMore(),
-                  false,
-                  maybePull,
-                ));
-              });
-
-          maybePull = () {
-            if (!shouldPullMore(1)) return Future.value();
-            return doPull(1);
-          };
-
-          if (shouldPullMore(initialDemand)) {
-            doPull(initialDemand);
-          }
-
-          return OffsetIteratorFutureValue(
-            iterator.value.p(O.fold(
-              () => const FutureValue.loading(),
-              (v) => FutureValue.data(v),
-            )),
-            iterator.hasMore(),
-            initialDemand > 0,
-            maybePull,
-          );
-        });
+        atomWithParent(
+          iteratorOnlyAtom(create),
+          (get, parent) => iteratorFutureValue(
+            get,
+            parent,
+            initialDemand: initialDemand,
+          ),
+        );
 
 /// Listens to an [OffsetIterator], and updates the exposed
 /// [OffsetIteratorValue] whenever it changes.
